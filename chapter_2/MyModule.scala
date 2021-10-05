f33350509b95e640a0ab2eaa5f2f4aa4e4e358fb
Tomas Menito <tomasmenito@gmail.com>
2021-10-05 17:03:36 -0300

    Include factorial on MyModule

--- chapter_2/MyModule.scala
+++ chapter_2/MyModule.scala
@@ -3,11 +3,27 @@ object MyModule {
     if (n < 0) -n
     else n
 
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+
+    go(n, 1)
+  }
+
   private def formatAbs(x: Int): String = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
   }
 
-  def main(args: Array[String]): Unit =
+  private def formatFactorial(x: Int): String = {
+    val msg = "The factorial of %d is %d"
+    msg.format(x, factorial(x))
+  }
+
+  def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
+    println(formatFactorial(7))
+  }
 }
