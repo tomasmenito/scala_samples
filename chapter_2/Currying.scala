732011668dff8fdda5cfdd8e736fc4437b919e78
Tomas Menito <tomasmenito@gmail.com>
2021-10-05 18:02:03 -0300

    Currying implementation

---
+++ chapter_2/Currying.scala
@@ -0,0 +1,9 @@
+object Currying {
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => (b => f(a, b))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(curry((a: Int, b: Int) => a + b)(1)(2))
+  }
+}
