b9a6919596489f80935088061440497c02ebd47c
Tomas Menito <tomasmenito@gmail.com>
2021-10-06 14:14:27 -0300

    Uncurry implementation

--- chapter_2/Currying.scala
+++ chapter_2/Currying.scala
@@ -1,9 +1,13 @@
 object Currying {
   def curry[A, B, C](f: (A, B) => C): A => (B => C) =
     a => (b => f(a, b))
-  }
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
 
   def main(args: Array[String]): Unit = {
     println(curry((a: Int, b: Int) => a + b)(1)(2))
+    println(uncurry((a: Int) => ((b: Int) => a + b))(1, 2))
   }
+
 }
